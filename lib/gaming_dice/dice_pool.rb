13ca6d98b0c108af1879f9464d008a344386d519
Justin Piotroski <justin.piotroski@gmail.com>
2020-06-07 16:57:39 -0700

    Modify dice pools to have different roll result rule options

--- lib/gaming_dice/dice_pool.rb
+++ lib/gaming_dice/dice_pool.rb
@@ -5,57 +5,85 @@ module GamingDice
   class DicePool
     extend Forwardable
 
+    # Possible options for rule to use for returning results
+    RULES = %i[each sum worst best].freeze
+
     # The Dice objects managed by this pool
     attr_accessor :dice
-    # Determines whether the dice should be handled seperately or together
-    attr_accessor :discrete
+    # Determines the rule for roll results
+    attr_reader :rule
 
-    def_delegators :@dice, :each, :count, :map
+    def_delegators :@dice, :each, :count, :map, :<<
 
     # Uses the +initarg+ to populate the pool
-    def initialize(initarg = nil, discr = false)
-      if initarg.is_a?(String)
-        @dice = Dice.call(initarg).flatten
-      elsif initarg.is_a?(Array)
+    def initialize(initarg = nil, rul = :sum)
+      if initarg.is_a?(Array)
         @dice = initarg
       elsif initarg.is_a?(Dice)
         @dice = Array(initarg)
       end
 
-      @discrete = discr
+      @rule = rul
     end
 
-    # Rolls all members of dice
+    # Sets the rule to +nu_rule+ if it exists
+    def rule=(nu_rule)
+      unless RULES.include?(nu_rule)
+        str = %("#{nu_rule}")
+        str << ' is not one of '
+        str << RULES.map(&:inspect).join(', ')
+
+        raise ArgumentError, str
+      end
+
+      @rule = nu_rule
+    end
+
+    # Returns the roll result depending on the rule
     def roll
-      discrete? ? roll_all : total
+      case rule
+      when :each
+        roll_all
+      when :sum
+        total
+      when :worst
+        take_worst
+      when :best
+        take_best
+      end
     end
 
-    # Returns individual roll results per dice in the pool
+    # Returns individual roll results per dice in the pool. Used with :each
     def roll_all
       dice.map(&:roll)
     end
 
-    # Rolls and then sums the rolls
+    # Rolls and then sums the rolls. Used with the :sum rule
     def total
       roll_all.reduce(&:+)
     end
 
-    # Predicate method for the discrete attribute
-    def discrete?
-      !!discrete
+    # Returns the best result of all dice rolls. Used with the :best rule
+    def take_best
+      roll_all.max
     end
 
-    # Slightly unusual behavior. When the comparator methods are called,
-    # the Integer() method is called on self and +other+.
-    # This dice thus uses a randomly generated roll to compare.
-    def <=>(other)
-      Integer(self) <=> Integer(other)
+    # Returns the worst result of all dice rolls Used with the :worst rule
+    def take_worst
+      roll_all.min
     end
 
     # Implicit conversion method returns a roll result, allowing you to compare
-    # the results of rolling a dice against a target number or other dice.
+    # the results of rolling these dice against a target number or other dice.
     def to_int
+      raise ArgumentError, 'Would return an Array' unless returns_one?
+
       roll
+    end
+
+    # True if a single integer result will be returned by this pool
+    def returns_one?
+      rule != :each
     end
   end
 end
